c0344b94a4efd5193a682c741fc2d96fd63ac4d2
rajat <rajat.k@sphinxworldbiz.co>
2023-05-25 07:57:09 +0530

    Home page, blog details, single blog  page, category, contact  templates created

---
+++ src/app/[pageSlug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+        <h1>page template</h1>
+    </>
+  )
+}
+
+export default Page
---
+++ src/app/blog/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <h1>Blog Details Pages</h1>
+    </>
+  )
+}
+
+export default page
